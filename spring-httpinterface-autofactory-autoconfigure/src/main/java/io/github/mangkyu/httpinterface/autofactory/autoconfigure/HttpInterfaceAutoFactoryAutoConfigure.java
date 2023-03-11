411aebb4fdcd980c54bdd61d913a0cdd0d5ed82a
MangKyu <whalsrb1226@gmail.com>
2023-03-11 22:53:08 +0900

    Add autoconfigure module

---
+++ spring-httpinterface-autofactory-autoconfigure/src/main/java/io/github/mangkyu/httpinterface/autofactory/autoconfigure/HttpInterfaceAutoFactoryAutoConfigure.java
@@ -0,0 +1,13 @@
+/*
+ *
+ *  HttpInterfaceAutoFactoryAutoConfigure.java 2023-03-11
+ *
+ *  Copyright 2023 WorksMobile Corp. All rights Reserved.
+ *  WorksMobile PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ */
+
+package io.github.mangkyu.httpinterface.autofactory.autoconfigure;
+
+public class HttpInterfaceAutoFactoryAutoConfigure {
+}
